1a077ca4ba11720c824d057435f275acaf2240fe
Sri-Nidhi-25 <140502551+Sri-Nidhi-25@users.noreply.github.com>
2024-11-15 20:50:55 +0530

    1st component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Temp from "./Basics.js";
+
+function App() {
+  return (
+    <div>
+      <Temp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Basics.js
@@ -0,0 +1,53 @@
+import React from "react";
+// import ReactDOM from "react-dom";
+
+// function Navbar(){
+//     return (
+//     <nav>
+//         <h1>Bob's Bistro</h1>
+//         <ul>
+//             <li>Menu</li>
+//             <li>About</li>
+//             <li>Contact</li>
+//         </ul>
+//     </nav> )
+// }
+
+// ReactDOM.render(navbar, document.getElementById("root"))
+
+// // instead below
+
+// ReactDOM.createRoot(document.getElementById("root")).render(navbar)
+
+// // a short form  fro the long form below
+
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(navbar)
+
+// export default Navbar;
+
+/**
+Challenge: 
+
+Part 1: Create a page of your own using a custom Page component
+
+It should return an ordered list with the reasons why you're
+excited to be learning React :)
+
+Render your list to the page
+
+ */
+function Temp() {
+  return (
+    <div>
+      <img src="./logo192.png" alt="react" />
+      <h1>Title</h1>
+      <ol>
+        <li>love it</li>
+        <li>need it</li>
+      </ol>
+    </div>
+  );
+}
+
+export default Temp;
